7e10b6af3b4ff510f39f5936299773cd73c3f4d5
Amr Abdelfattah <amrabdelfattah321@gmail.com>
2023-08-12 19:35:04 -0700

    all

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * Description: a program that prints all single digit numbers
+ * Return: 0 (Success)
+*/
+
+int main(void)
+{
+	int digit = 0;
+
+	while (digit < 10)
+	{
+		putchar(digit);
+		digit++;
+	}
+	putchar('\n');
+
+	return (0);
+}
