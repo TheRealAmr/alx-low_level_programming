b1bd094d65d700d806973654af1025e1354e13c1
Amr Abdelfattah <amrabdelfattah321@gmail.com>
2023-08-12 20:04:30 -0700

    all

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * Description:  prints all the numbers of base 16 in lowercase
+ * Return: 0 (Success)
+*/
+
+int main(void)
+{
+	char numbers = 0;
+	char letters = 'a';
+
+	while (numbers <= 9)
+	{
+		putchar(numbers);
+		numbers++;
+	}
+	while (letters <= 'f')
+	{
+		putchar(letters);
+		letters++;
+	}
+
+	putchat('\n');
+	return (0);
+}
---
+++ 0x01-variables_if_else_while/8-print_base16.c~
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * Description: print the alphabet letters in lowercase
+ * Return: 0 (Success)
+*/
+
+int main(void)
+{
+	char ch = 'z';
+
+	while (ch >= 'a')
+	{
+		putchar(ch);
+		ch--;
+	}
+	putchar('\n');
+
+	return (0);
+}
