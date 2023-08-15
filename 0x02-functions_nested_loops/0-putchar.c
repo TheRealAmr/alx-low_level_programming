5f9ac5f03ad24ec5c7c5bac7d93e0344076c5f74
Amr Abdelfattah <amrabdelfattah321@gmail.com>
2023-08-15 14:32:47 -0700

    all

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * main - entry point
+ * Description: a program that prints _putchar
+ * Return: 0 (Success)
+*/
+
+int main(void)
+{
+	char str[] = "_putchar";
+
+	for (int text = 0; text < 8; text++)
+		_putchar(str[text]);
+	_putchar('\n');
+
+	return (0);
+}
