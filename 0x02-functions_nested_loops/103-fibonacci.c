27f2491e096fc347a9ab3cad7b9a2f9e516924aa
Amr Abdelfattah <amrabdelfattah321@gmail.com>
2023-08-16 13:43:27 -0700

    all

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - Prints the sum of even-valued Fibonacci sequence
+ *        terms not exceeding 4000000.
+ *
+ * Return: Always 0.
+ */
+
+int main (void)
+{
+	int t1 = 1;
+	int t2 = 2;
+	int next;
+	int sum = 2;
+
+	while (next <= limit)
+	{
+		next = t1 + t2;
+		if (next % 2 == 0)
+			sum = sum + next;
+		t1 = t2;
+		t2 = t1;
+	}
+	printf("%d\n", sum);
+	return (0);
+}
