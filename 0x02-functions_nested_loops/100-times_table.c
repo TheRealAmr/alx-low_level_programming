f3a2f5140d6292f1651b9406b15a3d6e1fa01d41
Amr Abdelfattah <amrabdelfattah321@gmail.com>
2023-08-16 07:51:52 -0700

    all

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -6,26 +6,33 @@
 
 void print_times_table(int n)
 {
-	int num, mult, prod;
+        int num, mult, prod;
 
-	for (num = 0; num <= n; num++)
-	{
-		_putchar('0');
+        if (n > 15 || n < 0)
+        {
+            return (0);
+        }
+        else
+        {
+            for (num = 0; num <= n; num++)
+            {
+                    _putchar('0');
 
-		for (mult = 1; mult <= n; mult++)
-		{
-			_putchar(',');
-			_putchar(' ');
+                    for (mult = 1; mult <= n; mult++)
+                    {
+                            _putchar(',');
+                            _putchar(' ');
 
-			prod = num * mult;
+                            prod = num * mult;
 
-			if (prod <= n)
-				_putchar(' ');
-			else
-				_putchar((prod / 10) + '0');
+                            if (prod <= n)
+                                    _putchar(' ');
+                            else
+                                    _putchar((prod / 10) + '0');
 
-			_putchar((prod % 10) + '0');
-		}
-		_putchar('\n');
-	}
+                            _putchar((prod % 10) + '0');
+                    }
+                    _putchar('\n');
+            }
+        }
 }
