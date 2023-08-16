5d17c17bba93271ec06c2e50a1c3ed8da9fc9777
Amr Abdelfattah <amrabdelfattah321@gmail.com>
2023-08-16 14:02:44 -0700

    all

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -2,29 +2,31 @@
 #include <stdio.h>
 
 /**
- * main - Prints first 50 Fibonacci numbers, starting with 1 and 2,
- *        separated by a comma followed by a space.
+ * main - Prints the sum of even-valued Fibonacci sequence
+ *        terms not exceeding 4000000.
  *
  * Return: Always 0.
  */
 
 int main(void)
 {
-	int count;
 	unsigned long fib1 = 0, fib2 = 1, sum;
+	float total;
 
-	for (count = 0; count < 50; count++)
+	while (1)
 	{
 		sum = fib1 + fib2;
-		printf("%lu", sum);
+
+		if (sum > 4000000)
+			break;
+
+		if ((sum % 2) == 0)
+			total += sum;
 
 		fib1 = fib2;
 		fib2 = sum;
-
-		if (count == 49)
-			printf("\n");
-		else
-			printf(", ");
 	}
+	printf("%.0f\n", total);
+
 	return (0);
 }
