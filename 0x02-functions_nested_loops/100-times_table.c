f633d70b421354e146296bd77cd0fb1d33dd635f
Amr Abdelfattah <amrabdelfattah321@gmail.com>
2023-08-16 07:44:52 -0700

    all

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * times_table - Prints then n times table, starting with 0.
+ */
+
+void print_times_table(int n)
+{
+	int num, mult, prod;
+
+	for (num = 0; num <= n; num++)
+	{
+		_putchar('0');
+
+		for (mult = 1; mult <= n; mult++)
+		{
+			_putchar(',');
+			_putchar(' ');
+
+			prod = num * mult;
+
+			if (prod <= n)
+				_putchar(' ');
+			else
+				_putchar((prod / 10) + '0');
+
+			_putchar((prod % 10) + '0');
+		}
+		_putchar('\n');
+	}
+}
