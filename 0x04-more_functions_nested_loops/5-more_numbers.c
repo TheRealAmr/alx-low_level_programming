b7530d433e60c3ed2a143b285c5e2b62ad4b568d
Amr Abdelfattah <amrabdelfattah321@gmail.com>
2023-08-17 14:07:50 -0700

    all

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * more_numbers - nmbr fct
+ * Return: no return
+ */
+
+void more_numbers(void)
+{
+	int n;
+	int mult;
+
+	for (mult = 0; mult < 10; mult++)
+	{
+		for (n = 0; n <= 14; n++)
+		{
+		_putchar(n);
+		}
+
+		_putchar('\n');
+	}
+}
