61320eb6714157b7938c34b4ab42a6a355d8f198
Amr Abdelfattah <amrabdelfattah321@gmail.com>
2023-08-17 14:25:29 -0700

    all

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -10,13 +10,19 @@ void more_numbers(void)
 	int n;
 	int mult;
 
-	for (mult = 0; mult < 10; mult++)
+	for (mult = 1; mult <= 10; mult++)
 	{
 		for (n = 0; n <= 14; n++)
-		{
-		_putchar(n);
+  		{
+			if (n > 9)
+			{
+				_putchar(n / 10 + '0');
+			}
+
+		_putchar(n  % 10 + '0');
 		}
 
 		_putchar('\n');
 	}
+	_putchar('\n');
 }
