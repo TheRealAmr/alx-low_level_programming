22b3da6676561c83625ddad561c501e2a56e7323
Amr Abdelfattah <amrabdelfattah321@gmail.com>
2023-08-17 07:55:49 -0700

    all

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -12,6 +12,7 @@
 
 void print_remaining_days(int month, int day, int year)
 {
+
 	if ((year % 100 == 0 && year % 400 == 0) || (year % 4 == 0))
 	{
 		if (month >= 2 && day >= 60)
@@ -21,6 +22,7 @@ void print_remaining_days(int month, int day, int year)
 
 		printf("Day of the year: %d\n", day);
 		printf("Remaining days: %d\n", 366 - day);
+
 	}
 	else
 	{
