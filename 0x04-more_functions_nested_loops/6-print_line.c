189e8d2dc6524b0f8b0369168e77476fa6352bef
Amr Abdelfattah <amrabdelfattah321@gmail.com>
2023-08-17 14:52:50 -0700

    all

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_line - printing lines
+ * @n: number of times
+ *
+ * Return: void
+ */
+
+void print_line(int n)
+{
+	int x;
+
+	if (n <= 0)
+		_putchar('\n')
+	else
+	{
+		for (x = 1; x <= n; x++)
+			_putchar('_');
+		_putchar('\n');
+	}
+}
