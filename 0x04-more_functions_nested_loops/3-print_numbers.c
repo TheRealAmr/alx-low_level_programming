718cd688cd18c06167f1a46d00167b8376914231
Amr Abdelfattah <amrabdelfattah321@gmail.com>
2023-08-17 13:14:18 -0700

    all

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,14 +1,13 @@
 #include "main.h"
 
 /**
-*print_numbers - print 0123456789
-* owned by Bwave ICT
-*Return: void
+ * print_numbers - print 0123456789
+ * Return: void
 */
 
 void print_numbers(void)
 {
-	int n;
+	char n;
 
 	for (n = 0; n < 0; n++)
 	{
