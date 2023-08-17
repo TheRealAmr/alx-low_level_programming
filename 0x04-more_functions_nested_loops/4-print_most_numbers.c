b284532250fc698246d3fb960fb5e7440a49e189
Amr Abdelfattah <amrabdelfattah321@gmail.com>
2023-08-17 13:42:22 -0700

    all

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -5,20 +5,16 @@
  * Return: void
 */
 
-void print_most_numbers(void)
+oid print_most_numbers(void)
 {
 	char n;
 
 	for (n = 48; n < 58; n++)
 	{
 		if (n == 2 || n == 4)
-		{
 			continue;
-		}
-		else
-		{
-			_putchar(n);
-		}
+		_putchar(n + 48);
+
 	}
 
 	_putchar('\n');
