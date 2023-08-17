854bf96978f50ea2ad56d8d97452d6684402372a
Amr Abdelfattah <amrabdelfattah321@gmail.com>
2023-08-17 15:18:39 -0700

    all

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -18,7 +18,7 @@ void print_diagonal(int n)
 	{
 		for (x = 1; x <= n; x++)
 		{
-			for (space = 1; space <= n; space++)
+			for (space = 1; space <= x; space++)
 				_putchar(' ');
 			_putchar(92);
 			_putchar('\n');
