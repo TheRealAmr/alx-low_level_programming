77a8acfc09b70ffe02f419882ac38082a586384d
Amr Abdelfattah <amrabdelfattah321@gmail.com>
2023-08-21 14:48:08 -0700

    updating data

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * swap_int - swap the values of two integers
+ *
+ * @a: pointer to 1st value
+ * @b: pointer to 2nd value
+ */
+
+int _strlen(char *s)
+{
+	int count;
+
+	for (count = 0; s* != '\0'; ++count);
+    
+	return count;
+}
