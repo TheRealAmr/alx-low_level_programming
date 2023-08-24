b366e166b0e2e7f02c3ab58aeaf4df5612c753a1
Amr Abdelfattah <amrabdelfattah321@gmail.com>
2023-08-24 13:32:31 -0700

    all

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * *_strcat - a function that concatenates two strings
+ * @dest: destination
+ * @src: source
+ * Return: returns pointer to string dest
+*/
+
+char *_strcat(char *dest, char *src)
+{
+	strcpy(*dest, *src);
+	strcpy(*dest, '\0');
+
+	return (*dest);
+}
