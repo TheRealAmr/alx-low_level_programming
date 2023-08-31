d3e2373eb2cd4d3af6d9fc0f6fd7db4c7c474b04
Amr Abdelfattah <amrabdelfattah321@gmail.com>
2023-08-30 18:30:55 -0700

    All

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - a function that returns the length of a string
+ * @s: a string
+ * Return: void
+*/
+
+int _strlen_recursion(char *s)
+{
+	int n;
+	n = 0;
+
+	if (*s == '\0')
+	{
+		return (n);
+	}
+	else
+	{
+		n = n + 1;
+		_puts_recursion(s + 1);
+	}
+}
