3ef8ed5002c299c653a6b302a7e600e0454d6a5d
Amr Abdelfattah <amrabdelfattah321@gmail.com>
2023-09-01 18:04:15 -0700

    All

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main -  a program that prints its name
+ *         followed by a new line
+ *
+ * Return: 0 Success
+*/
+
+int main(int argc, char *argv[])
+{
+    printf("%s\n", argv[0]);
+
+    return (0);
+}
