355fcd62e169a4c7cbc3ad87fccdcf9e96a49f30
Amr Abdelfattah <amrabdelfattah321@gmail.com>
2023-09-05 20:06:20 -0700

    All

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,6 +1,5 @@
 #include "main.h"
 #include <stdlib.h>
-#include <stdio.h>
 
 /**
  * str_concat - concatenates two strings.
@@ -11,37 +10,37 @@
  * contains the contents of s1, followed by the contents of s2,
  * and null terminated. NULL on failure
  */
+
 char *str_concat(char *s1, char *s2)
 {
-	int i, j, len1, len2, len;
-	char *result;
+	_strcat(s1, s2);
 
-	len1 = len2 = 0;
+	char *news = NULL;
+	char *temp = s1;
 
-	if (s1 != NULL)
+	int i = 0;
+
+	while (temp[i] != '\0')
 	{
-		i = 0;
-		while (s1[i++] != '\0')
-			len1++;
+		i++;
 	}
 
-	if (s2 != NULL)
-	{
-		i = 0;
-		while (s2[i++] != '\0')
-			len2++;
-	}
+	news = (char *)malloc(sizeof(char) * i + 1);
 
-	len = len1 + len2;
-	result = (char *)malloc(sizeof(char) * (len + 1));
-	if (result == NULL)
+	int j;
+
+	if (news != NULL)
+	{
+		for (j = 0; s1[j] != '\0'; j++)
+		{
+			news[j] = s1[j];
+		}
+	}
+	else
+	{
 		return (NULL);
+	}
 
-	for (i = 0; i < len1; i++)
-		result[i] = s1[i];
-	for (j = 0; j < len2; j++, i++)
-		result[i] = s2[j];
-	result[len] = '\0';
-
-	return (result);
+	news[j] = '\0';
+	return (news);
 }
