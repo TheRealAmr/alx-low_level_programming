ab5147503e72ce4e2404c1977dbd6ad31f1418ad
Amr Abdelfattah <amrabdelfattah321@gmail.com>
2023-09-11 19:57:23 -0700

    All

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,17 @@
+#include "dog.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * new_dog - new dog
+ *
+ * @name: name's dog
+ * @age: age's dog
+ * @owner: owner's dog
+ *
+ * Return: newdog
+*/
+
+dog_t *new_dog(char *name, float age, char *owner)
+{
+}
