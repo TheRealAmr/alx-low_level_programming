96959985835361fa010348b4e855041e375f24af
Amr Abdelfattah <amrabdelfattah321@gmail.com>
2023-09-11 19:42:11 -0700

    All

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -8,7 +8,7 @@
  * @owner: and the owner
  *
  * Description: typing this bc it triggers the longer description
- */
+*/
 
 struct dog
 {
@@ -21,6 +21,5 @@ void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
-
 
 #endif
