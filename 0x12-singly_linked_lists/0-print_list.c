cf84ad7beed156cc51ef9a696dd2dee35a3a6884
Amr <amrabdelfattah321@gmail.com>
2023-09-22 18:31:07 -0700

    All

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -9,23 +9,19 @@
 
 size_t print_list(const list_t *h)
 {
-	size_t node_intervals;
+	unsigned int node;
 
-	node_intervals = 0;
+	node = 0;
 	while (h != NULL)
 	{
 		if (h->str == NULL)
-		{
-			printf("[%d] %s\n", 0, "(nil)");
-		}
+			printf("[0] (nil)\n");
 		else
-		{
 			printf("[%d] %s\n", h->len, h->str);
-		}
 
-		node_intervals++;
 		h = h->next;
+		node++;
 	}
 
-	return (node_intervals);
+	return (node);
 }
