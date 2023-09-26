964809f5b0785b2b6bd61f85271a5aa310377cbc
Amr <amrabdelfattah321@gmail.com>
2023-09-26 10:48:40 -0700

    All

---
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+#include <string.h>
+#include "lists.h"
+
+/**
+ * add_nodeint_end - function with 2 arguments
+ * @head: double pointer
+ * @n: string
+ * Return: NULL if failed, else address of start element
+*/
+
+listint_t *add_nodeint_end(listint_t **head, const int n)
+{
+	listint_t *last_node;
+	listint_t *temp;
+
+	last_node = malloc(sizeof(listint_t));
+
+	last_node->n = n;
+	last_node->next = NULL;
+
+	temp = head;
+	while (temp != NULL)
+		temp = temp->link;
+
+	temp->link = last_node;
+
+	return (last_node);
+}
